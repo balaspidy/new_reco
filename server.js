ce14686aaa7c6d45a3ae41303de51a57d3d4ec05
balaspidy <58440779+balaspidy@users.noreply.github.com>
2023-08-02 01:06:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,65 +1,65 @@
-const pptr = require('puppeteer');
+// puppeteerScript.js
+const puppeteer = require('puppeteer');
 
-(async () => {
-  // 1. Launch browser in headful mode so that we can click around and see how
-  // script works.
-  const browser = await pptr.launch({
-    headless: false,
-  });
-  const page = await browser.newPage();
+async function runPuppeteerScript(page) {
+  try {
 
-  const actions = [];
+    const actions = [];
 
-  // Function to handle redirect event
-  await page.exposeFunction('redirectEvent', async(info) => {
-    if (info.targetId == 'submit') {
+    // Function to handle redirect event
+    await page.exposeFunction('redirectEvent', async (info) => {
+      if (info.targetId == 'submit') {
         console.log(info.targetValue);
         await page.goto(info.targetValue);
-    }
-  });
-  await page.evaluateOnNewDocument(() => {
-    document.addEventListener('click', e => redirectEvent({
+      }
+    });
+    await page.evaluateOnNewDocument(() => {
+      document.addEventListener('click', e => redirectEvent({
         targetId: e.target.getAttribute("id"),
         targetValue: document.getElementById("url").value
-    }), true /* capture */);
-  });
-  await page.exposeFunction('reportEvent', info => {
-    console.log(info);
-    actions.push(info);
-  });
-  await page.evaluateOnNewDocument(() => {
-    document.addEventListener('click', e => reportEvent({targetName: e.target.baseURI, eventType: 'click'}), true /* capture */);
-  });
+      }), true /* capture */);
+    });
 
-  // Function to handle scroll event
-  await page.exposeFunction('scrollEvent', info => {
-    console.log(info);
-    actions.push(info);
-  });
-  await page.evaluateOnNewDocument(() => {
-    window.addEventListener('scroll', () => {
-      scrollEvent({
-        eventType: 'scroll',
-        scrollX: window.scrollX,
-        scrollY: window.scrollY,
+    await page.exposeFunction('reportEvent', info => {
+      console.log(info);
+      actions.push(info);
+    });
+    await page.evaluateOnNewDocument(() => {
+      document.addEventListener('click', e => reportEvent({ targetName: e.target.baseURI, eventType: 'click' }), true /* capture */);
+    });
+
+    await page.exposeFunction('reportEvent1', info => {
+      console.log(info);
+      actions.push(info);
+    });
+    await page.evaluateOnNewDocument(() => {
+      document.addEventListener('input', f => reportEvent1({ targetName: f.target.value, eventType: 'input' }), true /* capture */);
+    });
+
+    await page.exposeFunction('scrollEvent', info => {
+      console.log(info);
+      actions.push(info);
+    });
+    await page.evaluateOnNewDocument(() => {
+      window.addEventListener('scroll', () => {
+        scrollEvent({
+          eventType: 'scroll',
+          scrollX: window.scrollX,
+          scrollY: window.scrollY,
+        });
       });
     });
-  });
 
-  // Function to handle search event
-  await page.exposeFunction('searchEvent', info => {
-    console.log(info);
-    actions.push(info);
-  });
-  await page.evaluateOnNewDocument(() => {
-    const searchInput = document.getElementById('searchBar'); // Replace 'searchBar' with the actual ID of your search bar input element
-    searchInput.addEventListener('input', () => {
-      searchEvent({
-        eventType: 'search', 
-        searchQuery: searchInput.value,
-      });
-    });
-  });
+    await page.goto('file:///Users/balamurug.palanisamy/Git_sneha/new_reco/recording.html');
 
-  await page.goto('file://E:/final pro/public/recording.html');
-})();
+  } catch (err) {
+    console.error('Error executing Puppeteer script:', err);
+  }
+}
+module.exports = runPuppeteerScript;
+
+
+
+
+
+
