a1f5ac3874d8a4430e8b3ba213b6718ebb431226
balaspidy <58440779+balaspidy@users.noreply.github.com>
2023-08-05 20:46:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,25 +4,29 @@ async function runPuppeteerScript(page) {
   try {
     const browser = page.browser();
 
-    const recordingPage = await page.browser().newPage();
-    await recordingPage.goto('file:///E:/xper_puppeteer_1/recording.html');
+    const recordingPage = await browser.newPage();
+    await recordingPage.goto('file:///Users/balamurug.palanisamy/Git_sneha/new_reco/recording.html');
 
-    await recordingPage.exposeFunction('redirectEvent', async (info) => {
-        if (info.targetId == 'submit') {
-          console.log(info.targetValue);
-          await recordingPage.goto(info.targetValue);
-        }
-      });
-      await recordingPage.evaluateOnNewDocument(() => {
-        document.addEventListener('click', e => redirectEvent({
+    await recordingPage.exposeFunction('redirectEvent1', async (info) => {
+      console.log('redirectEvent1 called:', info); // Log the info to see if the function is being called
+      if (info.targetId == 'submit') {
+        console.log(info.targetValue);
+        await recordingPage.goto(info.targetValue);
+      }
+    });
+
+    await recordingPage.evaluate(() => {
+      document.addEventListener('click', e => {
+        console.log('Click event triggered:', e); // Log the event to see if the click event is being triggered
+        window.redirectEvent1({
           targetId: e.target.getAttribute("id"),
           targetValue: document.getElementById("url").value
-        }), true /* capture */);
-      });
-  
-     
+        });
+      }, true /* capture */);
+    });
+
     // Navigate to the recording.html URL in a new tab
-    
+
   } catch (err) {
     console.error('Error executing Puppeteer script:', err);
   }
