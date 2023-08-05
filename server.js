2ce3484a6c8b34b717311abdc50f0beaaf278751
2040-SNEHA <104159038+2040-SNEHA@users.noreply.github.com>
2023-08-05 16:43:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,45 +1,25 @@
-
-const runPuppeteerScript = require('./server.js');
 const puppeteer = require('puppeteer');
-//const {open, click, type, submit} = require('@puppeteer/recorder');
-(async () => {
-    // 1. Launch browser in headful mode so that we can click around and see how
-    // script works.
-    const browser = await puppeteer.launch({
-      headless: false,
-    });
-    const page = await browser.newPage();
-  
-    const actions = [];
 
-    await page.exposeFunction('stopRecordingEvent', async (info) => {
-      if (info.targetId == 'stopRecordingBtn') {
-        console.log(info);
-        await stopRecording(page);
-      } 
+async function runPuppeteerScript(page) {
+  try {
+    const browser = await puppeteer.launch({ headless: false });
+    const [page] = await browser.pages();
+
+    // Attach event listener to the "Redirect" button
+    page.exposeFunction('redirect', async (url) => {
+      const newTarget = await browser.waitForTarget(target => target.url() === 'about:blank');
+
+      const newPage = await browser.newPage();
+      await newPage.goto(url);
     });
-    
-    // Add a click event listener to the "Stop Recording" button
-    await page.evaluateOnNewDocument(() => {
-      document.addEventListener('click', e => stopRecordingEvent({
-        targetId: e.target.value
-      }), true /* capture */);
-    });
-  
-    // Function to handle redirect event
-    await page.exposeFunction('redirectEvent3', async(info) => {
-      if (info.targetId == 'Start Recording') {
-          console.log(info);
-          await runPuppeteerScript(page);
-      }
-    });
-    await page.evaluateOnNewDocument(() => {
-      document.addEventListener('click', e => redirectEvent3({
-          targetId: e.target.value
-      }), true /* capture */);
-    });
-  
-    await page.goto('file:///E:/xper_puppeteer_1/index2.html');
-    
-  })();
-  
+
+    // Load the local HTML file directly using Puppeteer
+    await page.goto('file:///E:/xper_puppeteer_1/recording.html');
+
+  } catch (err) {
+    console.error('Error executing Puppeteer script:', err);
+  }
+}
+
+// Export the runPuppeteerScript function
+module.exports = runPuppeteerScript;
