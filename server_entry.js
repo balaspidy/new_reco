2882d02ff82a94b04c3cced0d8c6d9e83dce0ed8
2040-SNEHA <104159038+2040-SNEHA@users.noreply.github.com>
2023-08-05 16:44:10 +0530

    Update and rename server_entry to server_entry.js

--- server_entry.js
+++ server_entry.js
@@ -1,6 +1,7 @@
-// otherFile.js
+
 const runPuppeteerScript = require('./server.js');
 const puppeteer = require('puppeteer');
+//const {open, click, type, submit} = require('@puppeteer/recorder');
 (async () => {
     // 1. Launch browser in headful mode so that we can click around and see how
     // script works.
@@ -10,6 +11,20 @@ const puppeteer = require('puppeteer');
     const page = await browser.newPage();
   
     const actions = [];
+
+    await page.exposeFunction('stopRecordingEvent', async (info) => {
+      if (info.targetId == 'stopRecordingBtn') {
+        console.log(info);
+        await stopRecording(page);
+      } 
+    });
+    
+    // Add a click event listener to the "Stop Recording" button
+    await page.evaluateOnNewDocument(() => {
+      document.addEventListener('click', e => stopRecordingEvent({
+        targetId: e.target.value
+      }), true /* capture */);
+    });
   
     // Function to handle redirect event
     await page.exposeFunction('redirectEvent3', async(info) => {
@@ -24,6 +39,7 @@ const puppeteer = require('puppeteer');
       }), true /* capture */);
     });
   
-    await page.goto('file:///Users/balamurug.palanisamy/Git_sneha/new_reco/index.html');
+    await page.goto('file:///E:/xper_puppeteer_1/index2.html');
+    
   })();
   
